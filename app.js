1e6f31eda1771896861f508a0f604b2116e60b94
Rajesh <rajeshrajatv@gmail.com>
2023-02-05 15:39:17 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// Import the `http` module
+const http = require('http');
+
+// Create an HTTP server that listens on port 3000
+const server = http.createServer((req, res) => {
+  // Write the response
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, World!');
+});
+
+// Start the server
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
